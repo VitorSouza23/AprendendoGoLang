ead5e46f78685506297e6c183bc55bb86c42a74b
Vitor de Souza <vitordesolza@gmail.com>
2020-11-14 19:38:51 -0300

    Primeiros passos com Go Lang

---
+++ Exercicio1/Exer1.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	x := 42
+	y := "James Bond"
+	z := true
+
+	//Mema linha
+	fmt.Println(x, y, z)
+
+	//Multiplas linhas
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+}
