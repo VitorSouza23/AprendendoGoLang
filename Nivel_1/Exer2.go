520a6cbf6f03284845fcd17a6737e7a9b9ee1da3
Vitor de Souza <vitordesolza@gmail.com>
2020-11-16 20:43:02 -0300

    Adição do exer 2

---
+++ Nivel_1/Exer2.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+var x int
+var y string
+var z bool
+
+func main() {
+	fmt.Println("Valor zero dos tipos básicos de GO:")
+
+	fmt.Println("int: ", x)
+	fmt.Println("string: ", y)
+	fmt.Println("bool: ", z)
+}
