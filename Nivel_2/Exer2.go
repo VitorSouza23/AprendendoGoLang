32eaf45113f40cb74df5340ef8d8ba844da43857
Vitor de Souza <vitordesolza@gmail.com>
2020-11-30 22:02:55 -0300

    Adicionando exercícios do capítulo 2

---
+++ Nivel_2/Exer2.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main()  {
+	a := (10 == 10)
+	b := (10 != 10)
+	c := (10 <= 10)
+	d := (10 >= 10)
+	e := (10 > 10)
+	f := (10 < 10)
+
+	fmt.Printf("a: %v\n", a)
+	fmt.Printf("b: %v\n", b)
+	fmt.Printf("c: %v\n", c)
+	fmt.Printf("d: %v\n", d)
+	fmt.Printf("e: %v\n", e)
+	fmt.Printf("f: %v\n", f)
+}
